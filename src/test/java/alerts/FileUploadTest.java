d45ea96b61f30116a582b0557e5481dc9d8693d7
nenadpetrov <nenad.petrovic@arithnea.de>
2020-02-12 14:41:52 +0100

    Commiting Angies project to Github

--- src/test/java/alerts/FileUploadTest.java
+++ src/test/java/alerts/FileUploadTest.java
@@ -10,7 +10,7 @@ public class FileUploadTest extends BaseTest {
     @Test
     public void testFileUpload() {
     var uploadPage = homePage.clickFileUpload();
-    uploadPage.uploadFile("C:\\Users\\nenad.petrovic\\Desktop\\Angie Jones Github\\Selenium WebDriver Project\\src\\test\\resources\\chromedriver.exe");
+    uploadPage.uploadFile("C:\\Users\\nenad.petrovic\\Desktop\\Automation\\Angie Jones Github\\Selenium WebDriver Project\\src\\test\\resources\\chromedriver.exe");
     assertEquals(uploadPage.getUploadedFiles(),"chromedriver.exe", "Uploaded file not correct");
     }
 }
